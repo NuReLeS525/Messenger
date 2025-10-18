30e3e92fd9673562ece0d986e7776893a387eab4
Nureles <tashbolotovnureles@gmail.com>
2025-10-18 12:31:52 +0600

    blanked

--- cm.py
+++
@@ -1,8 +0,0 @@
-import socket  
-hostname = socket.gethostname()  
-ip_address = socket.gethostbyname(hostname)  
-print(f"IP-адрес {hostname}: {ip_address}")  
-
-import os
-name = os.getlogin()
-print(name)
